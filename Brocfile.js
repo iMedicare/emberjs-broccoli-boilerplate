29aa3ccf527c48e693504b758a5df0c600d53cf6
Stas SUȘCOV <stas@net.utcluj.ro>
2014-04-28 19:29:03 +0300

    Rewrote Brocfile to be compatible with latest broccoli.

--- Brocfile.js
+++ Brocfile.js
@@ -1,155 +1,140 @@
-module.exports = function (broccoli) {
-  var filterCoffeeScript = require('broccoli-coffee');
-  var filterLess = require('broccoli-less');
-  var filterTemplates = require('broccoli-template');
-  var uglifyJavaScript = require('broccoli-uglify-js');
-  var compileES6 = require('broccoli-es6-concatenator');
-  var pickFiles = require('broccoli-static-compiler');
-  var concatFiles = require('broccoli-concat');
-  var env = process.env.BROCCOLI_ENV || 'development';
+var mergeTrees = require('broccoli-merge-trees');
+var findBowerTrees = require('broccoli-bower');
+var filterCoffeeScript = require('broccoli-coffee');
+var filterLess = require('broccoli-less');
+var filterTemplates = require('broccoli-template');
+var uglifyJavaScript = require('broccoli-uglify-js');
+var compileES6 = require('broccoli-es6-concatenator');
+var pickFiles = require('broccoli-static-compiler');
+var concatFiles = require('broccoli-concat');
 
-  function preprocess (tree) {
-    tree = filterTemplates(tree, {
-      extensions: ['hbs', 'handlebars'],
-      compileFunction: 'Ember.Handlebars.compile'
-    });
-    tree = filterCoffeeScript(tree, {
-      bare: true
-    });
-    tree = filterLess(tree, {
-      compress: env === 'production',
-      paths: ['.', './stylesheets', './vendor/bootstrap/less']
-    });
-    return tree;
-  }
+var bowerTrees = findBowerTrees();
+var env = process.env.BROCCOLI_ENV || 'development';
 
-  //
-  // Static
-  //
-  var publicTree = broccoli.makeTree('public');
-
-  //
-  // Application
-  //
-  var appTree = broccoli.makeTree('app');
-  var configTree = broccoli.makeTree('config');
-  var vendorTree = broccoli.makeTree('vendor');
-  var appAndVendorTree = null;
-  var appJs = null;
-  var appFilesToAppend = [
-    'jquery.js',
-    'handlebars.js',
-    'ember.js',
-    'ember-data.js',
-    'ember-resolver.js',
-    'bootstrap.js',
-    'config/environment.js',
-    'config/environments/' + env + '.js'
-  ];
-
-  configTree = pickFiles(configTree, {
-    srcDir: '/',
-    destDir: 'config'
+function preprocess(tree) {
+  tree = filterTemplates(tree, {
+    extensions: ['hbs', 'handlebars'],
+    compileFunction: 'Ember.Handlebars.compile'
   });
-  configTree = preprocess(configTree);
-
-  appTree = pickFiles(appTree, {
-    srcDir: '/',
-    destDir: 'app'
+  tree = filterCoffeeScript(tree, {
+    bare: true
   });
-  appTree = preprocess(appTree);
-
-  appAndVendorTree = [appTree, configTree, vendorTree].concat(
-    broccoli.bowerTrees());
-  appAndVendorTree = new broccoli.MergedTree(appAndVendorTree);
-
-  appJs = compileES6(appAndVendorTree, {
-    loaderFile: 'loader.js',
-    ignoredModules: [
-      'ember/resolver'
-    ],
-    inputFiles: [
-      'app/**/*.js'
-    ],
-    legacyFilesToAppend: appFilesToAppend,
-    wrapInEval: env !== 'production',
-    outputFile: '/assets/app.js'
+  tree = filterLess(tree, {
+    compress: env === 'production',
+    paths: ['.', './stylesheets', './vendor/bootstrap/less']
   });
+  return tree;
+}
 
-  if (env === 'production') {
-    appJs = uglifyJavaScript(appJs, {
-      mangle: true,
-      compress: true
-    });
-  }
+//
+// Static
+//
+var publicTree = 'public';
 
-  //
-  // Stylesheets
-  //
-  var appCss = null;
-  var cssTree = broccoli.makeTree('stylesheets');
-  var cssFiles = [
-    'qunit.css',
-    'assets/app.css'
-  ];
+//
+// Stylesheets
+//
+var appCss = null;
+var cssTree = 'stylesheets';
+var cssFiles = [
+  'qunit.css',
+  'assets/app.css'
+];
 
-  appCss = pickFiles(cssTree, {
-    srcDir: '/',
-    files: ['app.less'],
-    destDir: 'assets'
+appCss = pickFiles(cssTree, {
+  srcDir: '/',
+  files: ['app.less'],
+  destDir: 'assets'
+});
+
+if (env !== 'test') {
+  cssFiles.shift();
+}
+
+appCss = mergeTrees([appCss].concat(bowerTrees), {overwrite: true});
+appCss = preprocess(appCss);
+
+appCss = concatFiles(appCss, {
+  inputFiles: cssFiles,
+  outputFile: '/assets/app.css'
+});
+
+
+//
+// Application
+//
+var appTree = 'app';
+var configTree = 'config';
+var vendorTree = 'vendor';
+var testsTree = 'tests';
+var appAndVendorTree = null;
+var appJs = null;
+var inputFiles = ['app/**/*.js'];
+var appFilesToAppend = [
+  'jquery.js',
+  'handlebars.js',
+  'ember.js',
+  'ember-data.js',
+  'ember-resolver.js',
+  'bootstrap.js',
+  'config/environment.js',
+  'config/environments/' + env + '.js'
+];
+var testFilesToAppend = [
+  'qunit/qunit/qunit.js',
+  'ember-qunit/dist/globals/main.js',
+  'jquery-mockjax/jquery.mockjax.js',
+  'faker/dist/faker.js',
+  'tests/test_helper.js'
+];
+
+configTree = pickFiles(configTree, {
+  srcDir: '/',
+  destDir: 'config'
+});
+configTree = preprocess(configTree);
+
+appTree = pickFiles(appTree, {
+  srcDir: '/',
+  destDir: 'app'
+});
+appTree = preprocess(appTree);
+
+testsTree = pickFiles(testsTree, {
+  srcDir: '/',
+  destDir: 'tests'
+});
+testsTree = preprocess(testsTree);
+
+appAndVendorTree = [appTree, configTree, vendorTree, publicTree];
+
+if (env === 'test') {
+  appAndVendorTree.unshift(testsTree);
+  inputFiles.unshift('tests/*/*.js');
+  appFilesToAppend = appFilesToAppend.concat(testFilesToAppend);
+}
+
+appAndVendorTree = mergeTrees(
+  appAndVendorTree.concat(bowerTrees),
+  {overwrite: true}
+);
+
+appJs = compileES6(appAndVendorTree, {
+  loaderFile: 'loader-js/loader.js',
+  ignoredModules: [
+    'ember/resolver'
+  ],
+  inputFiles: inputFiles,
+  legacyFilesToAppend: appFilesToAppend,
+  wrapInEval: env !== 'production',
+  outputFile: '/assets/app.js'
+});
+
+if (env === 'production') {
+  appJs = uglifyJavaScript(appJs, {
+    mangle: true,
+    compress: true
   });
+}
 
-  if (env !== 'test') {
-    cssFiles.shift();
-  }
-
-  appCss = new broccoli.MergedTree([appCss].concat(broccoli.bowerTrees()));
-  appCss = preprocess(appCss);
-
-  appCss = concatFiles(appCss, {
-    inputFiles: cssFiles,
-    outputFile: '/assets/app.css'
-  });
-
-  //
-  // Tests
-  //
-  var testsTree = broccoli.makeTree('tests');
-  var testsJs = null;
-  var testAppFilesToAppend = appFilesToAppend.concat([
-    'qunit/qunit/qunit.js',
-    'ember-qunit/dist/globals/main.js',
-    'jquery-mockjax/jquery.mockjax.js',
-    'faker/dist/faker.js',
-    'tests/test_helper.js'
-  ]);
-
-  testsTree = pickFiles(testsTree, {
-    srcDir: '/',
-    destDir: 'tests'
-  });
-  testsTree = preprocess(testsTree);
-
-  testsJs = new broccoli.MergedTree(
-    [appAndVendorTree, testsTree].concat(broccoli.bowerTrees()));
-  testsJs = compileES6(testsJs, {
-    loaderFile: 'loader.js',
-    ignoredModules: [
-      'ember/resolver'
-    ],
-    inputFiles: [
-      'app/**/*.js',
-      'tests/*/*.js'
-    ],
-    legacyFilesToAppend: testAppFilesToAppend,
-    outputFile: '/assets/app.js'
-  });
-
-  // If we are testing, return the compiled file with testing sources.
-  if (env === 'test') {
-    appJs = testsJs;
-  }
-
-  return [publicTree, appJs, appCss];
-
-};
+module.exports = mergeTrees([publicTree, appCss, appJs]);
